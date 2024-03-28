2bca450c5853659ce2f1e5963f626df80937fe67
nikhilkumar50 <nikhilkumarray9889@gmail.com>
2024-03-29 01:19:01 +0530

    homepage modification

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,15 +5,16 @@ const Home = () => {
   const user = useSelector((state) => state.user.user);
 
   return (
-    <div className="bg-primary text-primary min-h-screen flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">
-          Welcome, {user ? user.name : "Guest"}!
-        </h1>
-        <p className="text-lg mb-8">You are now logged in.</p>
-        
+    <>
+      <div className="bg-primary text-primary min-h-screen flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4">
+            Welcome, {user ? user.name : "Guest"}!
+          </h1>
+          <p className="text-lg mb-8">You are now logged in.</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
